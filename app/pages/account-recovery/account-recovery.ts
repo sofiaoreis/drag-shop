6f2f9bd50d26b67680c4d5ddf55dbac0aadd0284
João Morgado <jota94mfm@hotmail.com>
2016-04-17 14:45:32 +0100

    Recover account with e-mail verifications

--- app/pages/account-recovery/account-recovery.ts
+++ app/pages/account-recovery/account-recovery.ts
@@ -1,20 +1,33 @@
 import {Page, NavController, NavParams} from 'ionic-angular';
+import {FORM_DIRECTIVES, FormBuilder,  ControlGroup, Validators, AbstractControl} from 'angular2/common';
 import {AccountRecoverNumPage} from '../account-recover-num/account-recover-num';
 
 
 @Page({
-  templateUrl: 'build/pages/account-recovery/account-recovery.html'
+  templateUrl: 'build/pages/account-recovery/account-recovery.html',
+  directives: [FORM_DIRECTIVES]
 })
 export class AccountRecoveryPage {
-constructor(private nav: NavController, navParams: NavParams) {
-  this.nav = nav;
-}
 
-donePressed() {
-    this.nav.push(AccountRecoverNumPage, {});
+  recoverForm: ControlGroup;
+  username: AbstractControl;
+
+constructor(private nav: NavController, navParams: NavParams, fb: FormBuilder) {
+  this.nav = nav;
+  this.recoverForm = fb.group({
+          'username': ['', Validators.compose([Validators.required])]
+  });
+  this.username = this.recoverForm.controls['username'];
 }
 
   goBack() {
    this.nav.pop();
+ }
+
+ onSubmit(value: string): void {
+         if(this.recoverForm.valid) {
+             console.log('Submitted value: ', value);
+         }
+         this.nav.push(AccountRecoverNumPage, {});
  }
 }
