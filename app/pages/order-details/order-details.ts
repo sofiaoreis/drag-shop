9b978822b9f6f1f4eace557c0dd9e0087eaea387
João Morgado <jota94mfm@hotmail.com>
2016-05-07 20:47:39 +0100

    Products checkout working

--- app/pages/order-details/order-details.ts
+++ app/pages/order-details/order-details.ts
@@ -1,13 +1,18 @@
 import {Page, NavController, NavParams} from 'ionic-angular';
 import {Order} from '../../models/order'
+import {Product} from '../../models/product'
 
 @Page({
-  templateUrl: 'build/pages/order-details/order-details.html'
+  templateUrl: 'build/pages/order-details/order-details.html',
 })
 export class OrderDetails {
   order: Order;
 
   constructor(private nav: NavController, navParams: NavParams) {
     this.order = navParams.get('order');
+  }
+
+  processProduct(product: Product){
+    this.order.products[product.id].processed = true;
   }
 }
