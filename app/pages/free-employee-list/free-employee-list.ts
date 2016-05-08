c8f5e0061be44bbdbe61cba8f8a4b0e533cc104b
João Morgado <jota94mfm@hotmail.com>
2016-05-08 23:22:07 +0100

    Employee allocation working with some bugs

--- app/pages/free-employee-list/free-employee-list.ts
+++ app/pages/free-employee-list/free-employee-list.ts
@@ -2,6 +2,7 @@ import {Page, NavController, NavParams} from 'ionic-angular';
 import {Order} from '../../models/order'
 import {Employee} from '../../models/employee'
 import {EmployeesService} from '../../providers/employees-service/employees-service'
+import {OrderDetails} from '../order-details/order-details'
 
 @Page({
   templateUrl: 'build/pages/free-employee-list/free-employee-list.html',
@@ -19,5 +20,12 @@ export class FreeEmployeeList {
       err => {console.log(err);},
       () => {console.log("Finished fetching employees");}
     );
+  }
+
+  addEmployee(employee: Employee){
+    employee.busy = true;
+    let alocatedEmployee = employee.first_name + " " + employee.last_name;
+    this.order.employee = alocatedEmployee;
+    this.nav.push(OrderDetails,{order: this.order});
   }
 }
