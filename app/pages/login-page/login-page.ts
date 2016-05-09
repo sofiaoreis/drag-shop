7b20f572113ec9991076290905059061dbca58dc
JSantos92 <skyjgod@gmail.com>
2016-05-09 11:52:19 +0100

    Minor fixes

--- app/pages/login-page/login-page.ts
+++ app/pages/login-page/login-page.ts
@@ -1,8 +1,9 @@
-import {Page} from 'ionic-angular';
+import {Page, IONIC_DIRECTIVES} from 'ionic-angular';
 
 
 @Page({
-  templateUrl: 'build/pages/login-page/login-page.html'
+  templateUrl: 'build/pages/login-page/login-page.html',
+  directives: [IONIC_DIRECTIVES]
 })
 export class LoginPage {
   constructor() {
---
+++ app/pages/login-page/login-page.ts~
@@ -0,0 +1,11 @@
+import {Page} from 'ionic-angular';
+
+
+@Page({
+  templateUrl: 'build/pages/login-page/login-page.html'
+})
+export class LoginPage {
+  constructor() {
+
+  }
+}
