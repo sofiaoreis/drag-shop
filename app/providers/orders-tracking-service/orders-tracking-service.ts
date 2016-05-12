0d8d28be2eac001de6cfcc72eb3ebb73a95207d0
SofiaReis <sofiareis1994@gmail.com>
2016-05-12 01:09:50 +0100

    order tracking service

--- app/providers/orders-tracking-service/orders-tracking-service.ts
+++ app/providers/orders-tracking-service/orders-tracking-service.ts
@@ -1,6 +1,25 @@
 import {Injectable} from 'angular2/core';
 import {Http} from 'angular2/http';
 import {Platform} from 'ionic-angular';
-
+import {Message} from '../../models/orderTracker'
 import 'rxjs/Rx';
 import {Observable} from 'rxjs/Rx'
+
+
+@Injectable()
+export class OrderTrackingService {
+    ordersTrackingUrl: string;
+    constructor(private http: Http, platform: Platform) {
+      if(platform.is('android') && platform.is('cordova')){
+        this.ordersTrackingUrl = '/android_asset/www/orders_tracking.json';
+      }
+      else{
+        this.ordersTrackingUrl = '/orders_tracking.json';
+      }
+    }
+
+    getOrderList() {
+        return Observable.forkJoin(
+          this.http.get(this.ordersTrackingUrl).map(res => <Order[]> res.json()),
+        );
+    }
