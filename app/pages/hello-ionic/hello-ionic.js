3160e41e0a1ade34a3c9a24668ab44e6fe3ce2d8
PedroSama <futurewatertao@hotmail.com>
2016-05-17 17:24:56 +0100

    TS implemented and css fixes
    Managed to implement TS and also fixed some somethings on the css and
    html

--- app/pages/hello-ionic/hello-ionic.js
+++ app/pages/hello-ionic/hello-ionic.js
@@ -5,12 +5,27 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 var ionic_angular_1 = require('ionic-angular');
+var mostpurchased_service_1 = require('../../providers/mostpurchased-service/mostpurchased-service');
+var common_1 = require("angular2/common");
 var HelloIonicPage = (function () {
-    function HelloIonicPage() {
+    function HelloIonicPage(nav, navParams, MostPurchasedService) {
+        this.nav = nav;
+        this.MostPurchasedService = MostPurchasedService;
+        this.mostTrigger = "visited";
     }
+    HelloIonicPage.prototype.onPageDidEnter = function () {
+        var _this = this;
+        //get all messages from or to the current user
+        this.MostPurchasedService.getMostPurchasedList().subscribe(function (data) {
+            _this.mostpurchased = data;
+            console.log(_this.mostpurchased);
+        }, function (err) { console.log(err); }, function () { console.log("Finished fetching random crap"); });
+    };
     HelloIonicPage = __decorate([
         ionic_angular_1.Page({
-            templateUrl: 'build/pages/hello-ionic/hello-ionic.html'
+            templateUrl: 'build/pages/hello-ionic/hello-ionic.html',
+            providers: [mostpurchased_service_1.MostPurchasedService],
+            directives: [common_1.NgStyle]
         })
     ], HelloIonicPage);
     return HelloIonicPage;
