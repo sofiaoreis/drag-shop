50bd4047a5897b3e19e2e33126fff55449ece59d
tiagommferreira <tiagommferreira55@gmail.com>
2016-05-27 16:17:01 +0100

    changed client model

--- app/models/client.ts
+++ app/models/client.ts
@@ -4,6 +4,9 @@ export class Client {
   first_name: string;
   last_name: string;
   email: string;
+  birthday: string;
+  address: Array<{address_line: string, city: string, state: string, zip_code: string}>;
+  alternate_address: Array<{address_line: string, city: string, state: string, zip_code: string}>;
 
   constructor() {
 
