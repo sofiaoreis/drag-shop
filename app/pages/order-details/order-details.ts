2f1e00c2b87c0af7508c62544403e5bc737089d4
tiagommferreira <tiagommferreira55@gmail.com>
2016-05-30 22:37:46 +0100

    dummy order on order detail

--- app/pages/order-details/order-details.ts
+++ app/pages/order-details/order-details.ts
@@ -10,7 +10,36 @@ export class OrderDetails {
   order: Order;
 
   constructor(private nav: NavController, navParams: NavParams) {
-    this.order = navParams.get('order');
+    //this.order = navParams.get('order');
+    this.order = new Order();
+    this.order.id = 1;
+    this.order.client = "Paula Vasquez";
+    this.order.city = "Macon";
+    this.order.payment = "Payment option"
+    this.order.address = "6 Waywood Parkway";
+    this.order.status = "Status";
+    this.order.employee = "Carlos Fernandes";
+
+    let product1 = new Product();
+    product1.id = 1;
+    product1.name = "Potatoes";
+    product1.weight = 25;
+    product1.units = "Kg";
+    product1.price = 10;
+    product1.processed = false;
+
+    let product2 = new Product();
+    product2.id = 3;
+    product2.name = "Rice";
+    product2.weight = 5;
+    product2.units = "Kg";
+    product2.price = 15;
+    product2.processed = true;
+
+
+    let products = [product1, product2];
+
+    this.order.products = products;
   }
 
   processProduct(product: Product){
