8c379a91f60acdf5017880fab7509d6cbdc3e5a4
SofiaReis <sofiareis1994@gmail.com>
2016-05-30 14:27:58 +0100

    Little corrections

--- app/pages/app-settings/app-settings.ts
+++ app/pages/app-settings/app-settings.ts
@@ -1,12 +1,14 @@
-import {Page, IONIC_DIRECTIVES} from 'ionic-angular';
+import {Page, NavController, NavParams, ViewController} from 'ionic-angular';
 
 
 @Page({
   templateUrl: 'build/pages/app-settings/app-settings.html',
-  directives: [IONIC_DIRECTIVES]
 })
 export class AppSettings {
-  constructor() {
+
+  constructor(private nav: NavController, navParams: NavParams) {
+    //Set the default value of the messages trigger so that the correct segment is open on page load
 
   }
+
 }
