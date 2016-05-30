6338368f0da2c1bcd9f56cc5e9c2db57b6f4c3fa
SofiaReis <sofiareis1994@gmail.com>
2016-05-30 13:43:55 +0100

    app-settings conflicts resolved

--- app/app.ts
+++ app/app.ts
@@ -1,57 +1,3 @@
-<<<<<<< HEAD
-import 'es6-shim';
-import {App, IonicApp, Platform, MenuController} from 'ionic-angular';
-import {StatusBar} from 'ionic-native';
-import {HelloIonicPage} from './pages/hello-ionic/hello-ionic';
-import {ListPage} from './pages/list/list';
-import {CustomPage} from './pages/custom-page/custom-page';
-import{LoginPage} from './pages/login-page/login-page';
-import{AppSettings} from './pages/app-settings/app-settings';
-
-
-@App({
-  templateUrl: 'build/app.html',
-  config: {} // http://ionicframework.com/docs/v2/api/config/Config/
-})
-class MyApp {
-  // make HelloIonicPage the root (or first) page
-  rootPage: any = HelloIonicPage;
-  pages: Array<{title: string, component: any}>;
-
-  constructor(
-    private app: IonicApp,
-    private platform: Platform,
-    private menu: MenuController
-  ) {
-    this.initializeApp();
-
-    // set our app's pages
-    this.pages = [
-      { title: 'Hello Ionic', component: HelloIonicPage },
-      { title: 'Login', component: LoginPage },
-      { title: 'App Settings', component: AppSettings },
-      { title: 'My First List', component: ListPage },
-      { title: 'Custom Page', component: CustomPage }
-    ];
-  }
-
-  initializeApp() {
-    this.platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
-    });
-  }
-
-  openPage(page) {
-    // close the menu when clicking a link from the menu
-    this.menu.close();
-    // navigate to the new page if it is not the current page
-    let nav = this.app.getComponent('nav');
-    nav.setRoot(page.component);
-  }
-}
-=======
 import 'es6-shim';
 import {App, IonicApp, Platform, MenuController} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
@@ -66,6 +12,7 @@ import {MessageListPage} from "./pages/message-list/message-list";
 import {Statistics} from "./pages/statistics/statistics";
 import{LoginPage} from './pages/login-page/login-page';
 import {StoreList} from "./pages/store-list/store-list";
+import{AppSettings} from './pages/app-settings/app-settings';
 
 
 @App({
@@ -103,8 +50,10 @@ class MyApp {
       { title: 'Client Messages', component: MessageListPage, icon: 'chatbubbles' },
       { title: 'Order Tracking', component: ListPage, icon: 'basket' },
       { title: 'Products', component: ListPage, icon: 'barcode' },
-      { title: 'App Settings', component: ListPage, icon: 'settings' },
+      { title: 'App Settings', component: AppSettings, ListPage, icon: 'cog'},
+      { title: 'Setting', component: ListPage, icon: 'settings' },
       { title: 'Login', component: LoginPage, icon: 'power' },
+
       { title: 'Logout', component: AccountRecoveryPage, icon: 'power' }
     ];
   }
@@ -123,4 +72,3 @@ class MyApp {
     nav.setRoot(page.component);
   }
 }
->>>>>>> master
