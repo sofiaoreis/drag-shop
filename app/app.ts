cc241464b7f2b6228f24c7e91db527f80a48cd79
SofiaReis <sofiareis1994@gmail.com>
2016-05-30 13:24:47 +0100

    Conflicts login resolved

--- app/app.ts
+++ app/app.ts
@@ -1,55 +1,3 @@
-<<<<<<< HEAD
-import 'es6-shim';
-import {App, IonicApp, Platform, MenuController} from 'ionic-angular';
-import {StatusBar} from 'ionic-native';
-import {HelloIonicPage} from './pages/hello-ionic/hello-ionic';
-import {ListPage} from './pages/list/list';
-import {CustomPage} from './pages/custom-page/custom-page';
-import{LoginPage} from './pages/login-page/login-page';
-
-
-@App({
-  templateUrl: 'build/app.html',
-  config: {} // http://ionicframework.com/docs/v2/api/config/Config/
-})
-class MyApp {
-  // make HelloIonicPage the root (or first) page
-  rootPage: any = HelloIonicPage;
-  pages: Array<{title: string, component: any}>;
-
-  constructor(
-    private app: IonicApp,
-    private platform: Platform,
-    private menu: MenuController
-  ) {
-    this.initializeApp();
-
-    // set our app's pages
-    this.pages = [
-      { title: 'Hello Ionic', component: HelloIonicPage },
-      { title: 'Login', component: LoginPage },
-      { title: 'My First List', component: ListPage },
-      { title: 'Custom Page', component: CustomPage }
-    ];
-  }
-
-  initializeApp() {
-    this.platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
-    });
-  }
-
-  openPage(page) {
-    // close the menu when clicking a link from the menu
-    this.menu.close();
-    // navigate to the new page if it is not the current page
-    let nav = this.app.getComponent('nav');
-    nav.setRoot(page.component);
-  }
-}
-=======
 import 'es6-shim';
 import {App, IonicApp, Platform, MenuController} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
@@ -60,6 +8,7 @@ import {CustomPage} from './pages/custom-page/custom-page'
 import {OrderList} from './pages/order-list/order-list'
 import {HTTP_PROVIDERS} from 'angular2/http'
 import {MessageListPage} from "./pages/message-list/message-list";
+import{LoginPage} from './pages/login-page/login-page';
 
 @App({
   templateUrl: 'build/app.html',
@@ -96,6 +45,7 @@ class MyApp {
       { title: 'Order Tracking', component: ListPage, icon: 'basket' },
       { title: 'Products', component: ListPage, icon: 'barcode' },
       { title: 'App Settings', component: ListPage, icon: 'settings' },
+      { title: 'Login', component: LoginPage, icon: 'power' },
       { title: 'Logout', component: AccountRecoveryPage, icon: 'power' }
     ];
   }
@@ -114,4 +64,3 @@ class MyApp {
     nav.setRoot(page.component);
   }
 }
->>>>>>> master
