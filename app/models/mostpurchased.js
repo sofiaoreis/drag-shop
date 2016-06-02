b7dd1b5050efd2f4af8e38a414bf889fd73d676a
SofiaReis <sofiareis1994@gmail.com>
2016-06-02 10:00:59 +0100

    erasing js files

--- app/models/mostpurchased.js
+++
@@ -1,8 +0,0 @@
-var MostPurchased = (function () {
-    function MostPurchased() {
-        //this.content = obj.content;
-    }
-    return MostPurchased;
-})();
-exports.MostPurchased = MostPurchased;
-//# sourceMappingURL=mostpurchased.js.map
--- app/models/stores.js
+++
@@ -1,8 +0,0 @@
-var Store = (function () {
-    function Store() {
-        //this.content = obj.content;
-    }
-    return Store;
-})();
-exports.Store = Store;
-//# sourceMappingURL=stores.js.map
