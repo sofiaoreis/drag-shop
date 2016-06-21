7f3234dab004823c319d0f4878b2542016a3beeb
SofiaReis <sofiareis1994@gmail.com>
2016-06-22 00:12:04 +0100

    statistics

--- app/pages/orders-tracking/orders-tracking.ts
+++ app/pages/orders-tracking/orders-tracking.ts
@@ -1,4 +1,8 @@
 import {Page, NavController, NavParams} from 'ionic-angular';
+import {CHART_DIRECTIVES} from 'ng2-charts/ng2-charts';
+import {Component} from '@angular/core';
+import {CORE_DIRECTIVES, FORM_DIRECTIVES, NgClass} from '@angular/common';
+
 import {OrdersService} from '../../providers/orders-service/orders-service';
 import {Order} from '../../models/order';
 import {OrderSeeAllPipe} from '../../pipes/orders/order-status';
@@ -8,15 +12,22 @@ import {OrderPendingPipe} from '../../pipes/orders/order-status';
 import {OrderCanceledPipe} from '../../pipes/orders/order-status';
 import {OrderDetails} from '../order-details/order-details'
 
+
+
 @Page({
   templateUrl: 'build/pages/orders-tracking/orders-tracking.html',
+  selector: 'doughnut-chart-demo',
   providers: [OrdersService],
+  directives: [CHART_DIRECTIVES, NgClass, CORE_DIRECTIVES, FORM_DIRECTIVES],
   pipes:[OrderSeeAllPipe, OrderShippedPipe, OrderInTrafficPipe, OrderPendingPipe, OrderCanceledPipe]
 })
 export class OrdersTrackingPage {
 
 orders: Order[];
 ordersTrigger: string;
+public doughnutChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartData:number[] = [350, 450, 100];
+  public doughnutChartType:string = 'doughnut';
 
   constructor(private nav: NavController, navParams: NavParams, private orderTrackingService: OrdersService) {
       this.ordersTrigger = "all";
@@ -34,6 +45,14 @@ ordersTrigger: string;
 
   itemTapped(item){
     this.nav.push(OrderDetails,{order: item});
+  }
+
+  public chartClicked(e:any):void {
+    console.log(e);
+  }
+
+  public chartHovered(e:any):void {
+    console.log(e);
   }
 
 }
