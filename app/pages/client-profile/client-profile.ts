291b1fb265ee354f5efd201adde494fabcf46001
mariamiranda <maria.miranda725@gmail.com>
2016-06-29 11:17:30 +0100

    ultimas alterações

--- app/pages/client-profile/client-profile.ts
+++ app/pages/client-profile/client-profile.ts
@@ -3,18 +3,22 @@ import {Client} from '../../models/client'
 import {WishList} from '../../models/wishList'
 import {WishListService} from '../../providers/wishList-service/wishList-service'
 import {Product} from '../../models/product'
+import {OrdersService} from '../../providers/orders-service/orders-service'
+import {Order} from '../../models/order'
+import {OrderDetails} from '../order-details/order-details'
 
 @Page({
   templateUrl: 'build/pages/client-profile/client-profile.html',
-  providers: [WishListService]
+  providers: [WishListService, OrdersService]
 })
 export class ClientProfilePage {
   client: Client;
   wishList: WishList;
   profileTrigger: string;
   products: Product[];
+  orders: Order[];
 
-  constructor(private nav: NavController, navParams: NavParams, private wishListService: WishListService) {
+  constructor(private nav: NavController, navParams: NavParams, private wishListService: WishListService, private ordersService: OrdersService) {
     if(navParams.get('trigger')) {
       this.profileTrigger = navParams.get('trigger');
     }
@@ -24,7 +28,7 @@ export class ClientProfilePage {
 
     this.client = navParams.get('client');
     this.wishList = navParams.get('wishList');
-    
+
 
   }
 
@@ -38,11 +42,27 @@ export class ClientProfilePage {
       err => {console.log(err);},
       () => {console.log("Finished fetching client WishList");}
   );
+  this.ordersService.getOrders().subscribe(
+    data => {
+      this.orders = data.filter(x=>(x.client_id== this.client.id && x.status==3));
+
+    },
+    err => {console.log(err);},
+    () => {console.log("Finished fetching client WishList");}
+);
+
+
   }
 
   goToProfile() {
     this.profileTrigger = "profile";
   }
+
+  goToOrders(item) {
+    this.nav.push(OrderDetails, {order:item});
+  }
+
+
 
 
 
